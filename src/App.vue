3999264fb3b9366dc74d921cab14ffdf168a2ff4
fajar.fa <fajar.fa@smoeets.com>
2017-01-24 20:28:08 +0700

    FIRST INIT

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <div id="app">
+    <sidebar>
+    </sidebar>
+  </div>
+</template>
+
+<script>
+import Sidebar from './components/Sidebar'
+export default {
+  name: 'app',
+  components: { Sidebar }
+}
+</script>
---
+++ src/components/Sidebar.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="sidebar">
+      
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'sidebar',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+
